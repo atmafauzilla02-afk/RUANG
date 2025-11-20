d5d88e2f958cb225c5f9f4bfc3e60bed30704cca
Mutothryyq <mutothryyq@gmail.com>
2025-11-20 12:28:16 +0700

    menambahkan proses login

---
+++ assets/aksi/proses_login.php
@@ -0,0 +1,32 @@
+<?php
+
+include '../../koneksi/koneksi.php';
+
+if (isset($_POST['submit'])) {
+
+    $nik = $_POST['nik'];
+    $password = $_POST['password'];
+
+    $query = mysqli_query($koneksi, "SELECT * FROM user WHERE nik='$nik' AND password='$password'");;
+    $cek = mysqli_num_rows($query);
+    $data = mysqli_fetch_array($query);
+
+    if ($cek > 0) {
+        session_start();
+        $_SESSION['nik'] = $data['nik'];
+        $_SESSION['role'] = $data['role'];
+
+        if ($data['role'] == 'warga') {
+            header("Location: ../../dashboard.php");
+        } elseif ($data['role'] == 'bendahara') {
+            header("Location: ../../dashboardBendahara.php");
+        } elseif ($data['role'] == 'rt') {
+            header("Location: ../../dashboardRT.php");
+        } else {
+            echo "<script>alert('Role tidak dikenali.'); window.location='../../login.php';</script>";
+        }
+    } else {
+        echo "<script>alert('NIK atau kata sandi salah.'); window.location='../../login.php';</script>";
+    }
+}
+?>
